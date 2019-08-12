7148ab6160f62690eb5d26747b32014ebe488700
Austin Rodgers <austin.rodgers@aon.com>
2019-08-12 17:04:16 -0400

    Update app.py
    yup

--- jobs/app.py
+++ jobs/app.py
@@ -12,9 +12,9 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql, values, commit, single):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    cursor = connection.execute(sql, values, commit, single)
+    cursor = connection.execute(sql, values)
 
     if commit == True:
         results = connection.commit()
